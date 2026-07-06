d9257257b6aef0cade61ce56714ebcda74641491
maidatlong <maidatlong@gmail.com>
2026-07-06 16:49:25 +0700

    Initial Commit

---
+++ src/main/java/mdl/proxysthproject/ProxySthProjectApplication.java
@@ -0,0 +1,13 @@
+package mdl.proxysthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxySthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxySthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mdl/proxysthproject/ProxySthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mdl.proxysthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxySthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
